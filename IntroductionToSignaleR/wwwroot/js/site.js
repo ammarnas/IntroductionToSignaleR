ca4e3ad0a0ef8071adb46e35c74524c408c53063
ammarnas <ammarnasser.contact@gmail.com>
2024-07-02 21:02:05 +0300

    download signalr and setup site.js

--- IntroductionToSignaleR/wwwroot/js/site.js
+++ IntroductionToSignaleR/wwwroot/js/site.js
@@ -1,4 +1,20 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-// Write your JavaScript code.
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + ": " + message;
+
+    var li = document.createElement("li");
+    li.textContent = msg;
+
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#txtMessage").val();
+
+    // this method is that in chatHub
+    connection.invoke("SendMessage", fromUser, message);
+})
